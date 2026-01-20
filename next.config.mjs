5e13527aeaa07e2c268c768d5ab7546764a66e97
Shah Nawaz <109155857+lyric9901@users.noreply.github.com>
2026-01-20 23:05:33 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-};
-
-export default nextConfig;
+async rewrites() {
+  return [
+    {
+      source: "/__/auth/:path*",
+      destination: `https://buzzme-5d7a7.firebaseapp.com/__/auth/:path*`,
+    },
+  ];
+}
